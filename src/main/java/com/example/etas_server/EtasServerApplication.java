ea5191e1dcdf898adf806fa0d7ec69b245d49409
Shakalyan <vdovitg@gmail.com>
2022-04-05 17:09:06 +0300

    Initial commit

---
+++ src/main/java/com/example/etas_server/EtasServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.etas_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtasServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtasServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/etas_server/EtasServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.etas_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtasServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
